813840fd5edd29124a25bb2b807d8553338a7066
Vladimir Spasic <vspasic@kayak.com>
2018-01-17 16:41:19 +0100

    handle favicon

--- router.js
+++ router.js
@@ -1,6 +1,11 @@
 const url = require('url')
 const Path = require('path-parser');
 
+function sendFavicon(params, req, res) {
+  res.writeHead(200)
+  res.end()
+}
+
 class Router {
   constructor() {
     this.routes = []
@@ -19,6 +24,10 @@ class Router {
 
     if (typeof req.method !== 'string' || typeof parsed.pathname !== 'string') {
       return {}
+    }
+
+    if (parsed.pathname.indexOf('favicon') > -1) {
+      return { handler: sendFavicon }
     }
 
     for (var i = this.routes.length - 1; i >= 0; i--) {
